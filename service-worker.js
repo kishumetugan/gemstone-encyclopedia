e1a1ca2c5f14842ff639fee0379eec9c2081b631
kishumetugan <kishumetugan@gmail.com>
2026-03-12 03:56:06 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'gemstone-encyclopedia-v1.0';
 const urlsToCache = [
-  '/gemstone-encyclopedia/',
-  '/gemstone-encyclopedia/index.html',
-  '/gemstone-encyclopedia/css/styles.css',
-  '/gemstone-encyclopedia/js/data.js',
-  '/gemstone-encyclopedia/js/app.js'
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/data.js',
+  './js/app.js'
 ];
 
 // התקנה - שמירת קבצים ב-Cache
@@ -21,11 +21,9 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // אם יש ב-Cache - החזר מהCache
         if (response) {
           return response;
         }
-        // אם אין - הורד מהרשת
         return fetch(event.request);
       })
   );
